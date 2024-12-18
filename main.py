b384dc4c1dbbbb5d0b45d62255217524f358749c
beaudrytinklerl <beaudrytinklerl@educ.college-montreal.qc.ca>
2024-12-18 14:55:09 -0500

    commit

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ Modèle de départ pour la programmation Arcade.
 Il suffit de modifier les méthodes nécessaires à votre jeu.
 """
 import arcade
+from random import randint, choice
+
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Modèle de départ"
+
+def random_number(randomness):
+    number = randint(0, randomness)
+    return number
 
 
 class MyGame(arcade.Window):
@@ -32,22 +38,42 @@ class MyGame(arcade.Window):
         """
         # C'est ici que vous allez créer vos listes de sprites et vos sprites.
         # C'est aussi ici que vous charger les sons de votre jeu.
-        pass
+        self.black = 0, 0, 0
+        self.blue = 0, 0, 255
+        self.brown = 177, 117, 89
+        self.dark_blue = 0, 0, 128
+        self.cyan = 0, 255, 255
+        self.gold = 255, 220, 70
+        self.green = 0, 255, 0
+        self.red = 255, 0, 0
+        self.grey = 180, 180, 180
+        self.orange = 255, 120, 0
+        self.pink = 255, 0, 128
+        self.purple = 170, 0, 255
+        self.silver = 240, 240, 235
+        self.violet = 238, 130, 238
+        self.white = 255, 255, 255
+        self.yellow = 255, 255, 0
+        self.salmon = 255, 76, 76
+        self.copper = 245, 175, 145
+        self.brass = 220, 175, 90
+        self.mandarin = 255, 156, 0
+        self.color_list = [self.black, self.dark_blue, self.blue, self.brown, self.cyan, self.gold, self.green, self.red, self.grey, self.orange, self.pink, self.purple, self.silver, self.violet, self.white, self.yellow, self.salmon, self.salmon, self.copper, self.brass, self.mandarin]
+        self.number_of_repeats = 0
 
     def on_draw(self):
         """
         C'est la méthode que Arcade invoque à chaque "frame" pour afficher les éléments
         de votre jeu à l'écran.
         """
-        for i in range(1, 20):
-            # Cette commande permet d'effacer l'écran avant de dessiner. Elle va dessiner l'arrière
-            # plan selon la couleur spécifié avec la méthode "set_background_color".
-            self.clear()
+        # Cette commande permet d'effacer l'écran avant de dessiner. Elle va dessiner l'arrière
+        # plan selon la couleur spécifié avec la méthode "set_background_color".
 
-            arcade.draw_circle_filled(10, 10, 20, (255, 54, 34))
-
-
-
+        while self.number_of_repeats <= 20:
+            self.random_color = choice(self.color_list)
+            self.circle = arcade.draw_circle_filled(random_number(800), random_number(600), random_number(100),(self.random_color))
+            self.color_list.remove(self.random_color)
+            self.number_of_repeats += 1
         # Invoquer la méthode "draw()" de vos sprites ici.
 
     def on_update(self, delta_time):
